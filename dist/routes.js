a155a9382b44240a5428ce3920d57d071fd6bc23
Andreia Trianon <andreiatrianon@outlook.com>
2018-09-26 22:42:45 -0300

    correcao da renderizacao de pagina quando categorias tiverem '/' no nome. Para isso, foi utilizado o 'replace' na funcao, substituindo esse caracter pelo '-'.

--- dist/routes.js
+++ dist/routes.js
@@ -21,7 +21,6 @@ function requestAPI(url, success) {
 function getCategories(data) {
   mainTemplateDefault('categories', 'Choose a Category');
   getEachCategory(data);
-
 }
 
 function mainTemplateDefault(id, title) {
@@ -48,7 +47,7 @@ function getEachCategory(data) {
       <div class="col s12 m6 l4">                            
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <a href="${Object.values(el)[0]}">
+            <a href="${Object.values(el)[0].replace(/\//g,'-')}">
               <img width="305" height="229" src="https://adbeus.com/wp-content/uploads/2016/11/s1odxcae9cjag71iye1c-305x229.jpg" class="responsive-img wp-post-image" alt="Noble Café" title="Noble Café" srcset="https://adbeus.com/wp-content/uploads/2016/11/s1odxcae9cjag71iye1c-305x229.jpg 305w, https://adbeus.com/wp-content/uploads/2016/11/s1odxcae9cjag71iye1c-300x225.jpg 300w, https://adbeus.com/wp-content/uploads/2016/11/s1odxcae9cjag71iye1c-768x576.jpg 768w, https://adbeus.com/wp-content/uploads/2016/11/s1odxcae9cjag71iye1c.jpg 800w" sizes="(max-width: 305px) 100vw, 305px" />
               <span class="card-title home">${Object.values(el)[0]}</span>
             </a>
@@ -60,7 +59,8 @@ function getEachCategory(data) {
 }
 
 function getRecipesList(ctx) {
-  let thisCategory = ctx.params.categoryName;
+  let categoryNameInURI = ctx.params.categoryName;
+  let thisCategory = categoryNameInURI.replace(/-/g,'/');
   mainTemplateDefault('recipes-list', thisCategory);
   let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' + thisCategory.replace(' ','_');
   let success = getEachRecipe;
@@ -126,18 +126,19 @@ function getIngredients(obj) {
 
 
 function getEachRecipe(data) {
+  let categoryNameToURI = $('.title').html().replace(/\//g,'-');
   data['drinks'].map((el, i) =>
     $('#recipes-list').append(`
       <div class="col s12 m6 l4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <a href="${$('.title').html()}/${data['drinks'][i]['strDrink']}">
+            <a href="${categoryNameToURI}/${data['drinks'][i]['strDrink']}">
             <img width="305" height="229" src="${data['drinks'][i]['strDrinkThumb']}" alt="${data['drinks'][i]['strDrink']}" title="${data['drinks'][i]['strDrink']}">
             </a>
           </div>
           <div class="card-content">
-            <p class="area"><a href="${$('.title').html()}/${data['drinks'][i]['strDrink']}">See recipe</a></p>
-            <a href="${$('.title').html()}/${data['drinks'][i]['strDrink']}" class="card-title activator brown-text text-darken-4">${data['drinks'][i]['strDrink']}</span></a>
+            <p class="area"><a href="${categoryNameToURI}/${data['drinks'][i]['strDrink']}">See recipe</a></p>
+            <a href="${categoryNameToURI}/${data['drinks'][i]['strDrink']}" class="card-title activator brown-text text-darken-4">${data['drinks'][i]['strDrink']}</span></a>
           </div>
         </div>
       </div>
